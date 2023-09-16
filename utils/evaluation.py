3c4ec53f0706f61355af2eb62f906fde8c961a82
avisinghal6 <avisinghal6@gmail.com>
2023-09-16 16:36:03 -0400

    added QNA

--- utils/evaluation.py
+++ utils/evaluation.py
@@ -3,9 +3,9 @@ import openai
 from openai import ChatCompletion
 from api_keys import OPENAPI_KEY
 
-import torch
+# import torch
 import json
-from sentence_transformers import util
+# from sentence_transformers import util
 
 
 openai.api_key = OPENAPI_KEY
@@ -94,3 +94,54 @@ def get_groups():
      groups.append(response[i])
 
   return groups
+
+def get_QNA(questions):
+    '''This function expects a list of questions, it will generate 10 new questions, the output will be a dict object
+    of the format new_questions={"questions":[{"question":"","answer":""},{},{}]}'''
+    # questions=["What is the purpose of an Integrated Development Environment (IDE) in the context of programming?",
+    #            "Can you explain the concept of 'variables' in programming and provide an example of declaring and initializing a variable?",
+    #            "What is the difference between a high-level programming language and a low-level programming language? Give examples of each.",
+    # ]
+    
+    new_questions={"questions":[]}
+    prompt="""Give a total of 10 questions, some of them should be of a level similar to the questions I have provided to you.
+           Some questions should be more difficult and some questions should be much harder. Ensure that the questions are relevant and help the 
+           reader learn. Be sure to provide the answer to the questions, do not repeat any question.  Give the output in the format: 
+           {{"questions":
+           [
+           {
+           "question": "","answer":""
+           },
+           {
+           "question": "","answer":""
+           },
+           ]
+           }}
+           I have provided the previous questions below:
+            """
+    question_prompt=""
+    for q in questions:
+        question_prompt+='\n'+q
+    
+    prompt+=question_prompt
+    response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "user", 
+         "content": prompt
+         }
+    ]
+    )
+
+    response=response["choices"][0]["message"]["content"]
+    response = response.replace("\\", "").replace("\n", "")
+    response=json.loads(response)
+    response=response["questions"]
+
+    for r in response:
+        r=json.dumps(r)
+        r=json.loads(r)
+        new_questions["questions"].append(r)
+    
+    return new_questions
+    
